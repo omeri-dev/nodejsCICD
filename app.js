a682f1b2b6bc0814fc422f76a8c96f92944b4961
omeridev <220051080+omeri-dev@users.noreply.github.com>
2025-08-12 16:02:37 +0000

    Test CI/CD by updating app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Node.js');
+  res.end('Hello from Node.js, sekarang sudah update dari loj 23.04');
 });
 
 server.listen(port, hostname, () => {
